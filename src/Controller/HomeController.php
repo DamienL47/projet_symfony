bc7672cbed9ed9c1f31e8c52be20c6dc83ed8799
DamienL47 <104761749+DamienL47@users.noreply.github.com>
2022-06-30 14:48:52 +0200

    Ajout des commentaire au code existant

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// définition de la classe controller pour la page home
 class HomeController
 {
+    // création de la route pour lui affecter une méthode
     /**
      * @Route("/")
      */
-
+// affectation de la méthode à la route pour informer le navigateur de ce qu'il doit afficher
     public function accueil()
     {
         return new Response('Bonjour');
