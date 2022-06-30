b1a577b96dad4b78736db75c83330c569840f1f3
DamienL47 <104761749+DamienL47@users.noreply.github.com>
2022-06-30 15:51:12 +0200

    Ajout de controller home qui renvoie un message twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // définition de la classe controller pour la page home
-class HomeController
+//ajout du extends AbstractController pour permettre l'héritage de classe
+class HomeController extends AbstractController
 {
     // création de la route pour lui affecter une méthode
     /**
@@ -15,6 +17,12 @@ class HomeController
 // affectation de la méthode à la route pour informer le navigateur de ce qu'il doit afficher
     public function accueil()
     {
-        return new Response('Bonjour');
+        //création de l'instance de classe
+        $name = 'Damien';
+        // lien vers le fichier twig qui renverra la vu demandée
+        return $this->render("nom.html.twig", [
+            //donne au fichier twig l'information à afficher
+            'name' => $name
+        ]);
     }
 }
